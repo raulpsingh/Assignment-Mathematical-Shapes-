2f58ab2b3af550b047e95f5a3a03471c464ab9d8
raulpsingh <57406645+raulpsingh@users.noreply.github.com>
2022-08-24 16:57:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let side = 9
+let d = side*Math.sqrt(2)
+console.log("Diagonal of a square is " + d)
+
+let side1 = 5
+let side2 = 6
+let side3 = 7
+
+
+let semiP = (5+6+7)/2
+let areaOfTriangle = Math.sqrt(semiP*(semiP-side1)*(semiP-side2)*(semiP-side3))
+console.log("Area of a triangle is " + areaOfTriangle)
+
+
+let radius = 4
+let circum = 2 * Math.PI * radius
+let areaOfCircle = Math.PI * Math.pow(radius,2)
+console.log("Circumference of circle is "+ circum + " and area is " + areaOfCircle)
